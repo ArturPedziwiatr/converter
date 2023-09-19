135674189891ffb60a711479c985f08a0d0761c4
Artur Pedziwiatr <Apedziwiatr@gsd.pl>
2023-09-19 09:45:46 +0200

    add error handling

--- src/error.py
+++ src/error.py
@@ -2,35 +2,31 @@ from src.logger import console
 from enum import Enum
 
 class Error(Enum):
-  def goCesiumTiler():
-    return { 
-      'name': 'Error with gocesiumtiler',
-      'code': '001'
-    }
-  def pdal():
-    return{ 
-      'name': 'Error with pdal',
-      'code': '002'
-    }
-  def wrongEpsg():
-    return { 
-      'name': 'Error with download EPSG',
-      'code': '003'
-    }
-  def notDefined(lab: str):
-    return {
-      'name': '{} is not defined'.format(lab),
-      'code': '004'
-    }
+  WrongEpsgError='001'
+  NotDefinde='002'
 
 class Throw(Exception):
-  def __init__(self, err: Error, msg: any, context: str):
-    if not msg:
-      msg = err.name
+  def __init__(self, context= '',  msg='', name='Unknown error', code='unknown'):
     console.error('[{}] | {}'.format(context, msg))
-    error = {
-      'name': err['name'],
-      'code': err['code'],
-      'msg': msg
-    }
-    super().__init__(error)
+    self.msg = msg
+    self.name = name
+    self.code = code
+    super().__init__(msg, context, name, code)
+
+class WrongEpsg(Throw):
+  def __init__(self, context, msg='Error with download EPSG'): 
+    super().__init__(
+      context,
+      msg,
+      Error.WrongEpsgError.name,
+      Error.WrongEpsgError.value
+    )
+
+class NotDefined(Throw):
+  def __init__(self, context, msg='Element is not defined'): 
+    super().__init__(
+      context,
+      msg,
+      Error.NotDefinde.name,
+      Error.NotDefinde.value
+    )
